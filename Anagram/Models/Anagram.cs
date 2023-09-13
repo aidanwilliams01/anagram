bb19bf13f5fb3459c23e73f71d6dca25cb217b6f
Aidan Williams <aidanwilliams01@gmail.com>
2023-09-13 15:55:43 -0700

    update files

--- Anagram/Models/Anagram.cs
+++ Anagram/Models/Anagram.cs
@@ -2,12 +2,6 @@ namespace ShapeTracker.Models
   {
     public class Rectangle
     {
-      private int _side1;
-      public int Side1
-      {
-        get { return _side1; }
-        set { _side1 = value; }
-      }
       public int Side2 { get; set; }
 
       public Rectangle(int length1, int length2)
